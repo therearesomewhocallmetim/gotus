eaa5fb64153d8fb43c2b4e5a7a04fe962d880764
Timofey Danshin <t.danshin@gmail.com>
2024-10-12 21:30:08 +0300

    Pass the existing tests for the simple impl

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -1,12 +1,54 @@
 package hw02unpackstring
 
+// package main
+
 import (
 	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/rivo/uniseg"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(str string) (string, error) {
+	// var lastChar string
+	canAcceptNumber := false
+	result := []string{}
+	// candidate := ""
+
+	graphemes := uniseg.NewGraphemes(str)
+
+	for graphemes.Next() {
+		grapheme := graphemes.Str()
+		n, err := strconv.Atoi(grapheme)
+		if err != nil { // the grapheme is not a number
+			result = append(result, grapheme)
+			canAcceptNumber = true
+			continue
+		}
+		if !canAcceptNumber {
+			fmt.Printf("We need char! %v, %d, %s", result, n, grapheme)
+			return "", ErrInvalidString
+		}
+		var toAppend string
+		result, toAppend = result[:len(result)-1], result[len(result)-1]
+		for i := 0; i < n; i++ {
+			result = append(result, toAppend)
+		}
+		canAcceptNumber = false
+
+	}
+	return strings.Join(result, ""), nil
 }
+
+// func main() {
+
+// 	s, err := Unpack("d̷̙͈̙̓͛̇ͨ̾̐4   i̧̻̪͍͒ͬ͆̀̒͝      a̷̯̞̻ͤ͏͙͙̜͘     c̨̭͚̼̙̍ͨ͌̚͝      r̨͇̯̱̍͋ͧͩ̕͜    i̭͍̘̞̣̱̐͛͜͟     t̢͇̙̯ͭ̐̇̽ͥ̈    三ï̴̮̺̜̙̪͉̩̮    c̰̞͍͖̪̣ͮ̂̚͝     ì̧̙̤́͂̽̑̚͡    s̗͕̘̩̯̎́̚̕͟      ṁ̫̩̭̊͐ͪ́̈͢   ")
+// 	if err != nil {
+// 		panic("Hell")
+// 	}
+// 	println(s)
+// }
